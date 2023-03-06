fea5b4e64f6c78c34a0299436d117e688a5845a5
Yuliia Nivina <y.nivina@gmail.com>
2023-03-06 22:59:26 +0200

    Add task-01

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const refs = {
+  body: document.querySelector('body'),
+  startBtn: document.querySelector('button[data-start]'),
+  stopBtn: document.querySelector('button[data-stop]'),
+};
+let timerId = null;
+
+refs.startBtn.addEventListener('click', onStartBtnClick);
+refs.stopBtn.addEventListener('click', onStopBtnClick);
+
+function onStartBtnClick(evt) {
+   timerId = setInterval(() => {
+       refs.body.style.backgroundColor = getRandomHexColor();
+       refs.startBtn.disabled = true;
+   }, 1000);
+};
+
+function onStopBtnClick(evt) {
+    clearInterval(timerId);
+    refs.startBtn.disabled = false;
+}
