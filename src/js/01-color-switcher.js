6f775572cf8d94f41e108119e4ab30d33fa2753e
Yuliia Nivina <y.nivina@gmail.com>
2023-03-07 23:40:34 +0200

    Check

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -12,14 +12,19 @@ let timerId = null;
 refs.startBtn.addEventListener('click', onStartBtnClick);
 refs.stopBtn.addEventListener('click', onStopBtnClick);
 
+refs.stopBtn.disabled = true;
+
 function onStartBtnClick(evt) {
    timerId = setInterval(() => {
        refs.body.style.backgroundColor = getRandomHexColor();
        refs.startBtn.disabled = true;
+       refs.stopBtn.disabled = false;
    }, 1000);
 };
 
 function onStopBtnClick(evt) {
     clearInterval(timerId);
     refs.startBtn.disabled = false;
+    refs.stopBtn.disabled = true;
 }
+console.log("hello");
---
+++ src/js/02-timer.js
@@ -0,0 +1,12 @@
+// import flatpickr from "flatpickr";
+// import "flatpickr/dist/flatpickr.min.css";
+
+// // flatpickr("#datetime-picker", {});
+// // const fp = flatpickr("#datetime-picker", {});
+// const myInput = document.querySelector('input');
+// const fp = flatpickr(myInput, {});
+// console.log(fp);
+// console.log(flatpickr);
+// console.log(myInput);
+console.log("hello");
+console.log("hello");
