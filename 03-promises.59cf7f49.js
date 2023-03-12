b81dd9128cc4c6d7ac01cf2e0df7666f9f433188
YuliiaNivina <112802335+YuliiaNivina@users.noreply.github.com>
2023-03-12 11:47:29 +0000

    Deploying to gh-pages from @ YuliiaNivina/goit-js-hw-09@bb6dbd568b06f462d8c3895c045c8295b9662bd0 🚀

---
+++ 03-promises.59cf7f49.js
@@ -0,0 +1,2 @@
+!function(){function e(e){e.preventDefault();var o=e.currentTarget.delay;console.log(o)}({form:document.querySelector(".form")}).form.addEventListener("submit",e),e(),console.log("hello")}();
+//# sourceMappingURL=03-promises.59cf7f49.js.map
